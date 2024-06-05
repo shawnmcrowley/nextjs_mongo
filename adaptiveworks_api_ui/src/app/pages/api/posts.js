c71df8c8ef6b37a7fe2e26393424998c0741c0a1
Shawn M. Crowley <scrowley@buffalo.edu>
2024-06-05 17:41:55 -0400

    Intial Commit

---
+++ adaptiveworks_api_ui/src/app/pages/api/posts.js
@@ -0,0 +1,32 @@
+// posts.js
+
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("nextjs-mongodb-demo");
+  switch (req.method) {
+    case "POST":
+      let bodyObject = JSON.parse(req.body);
+      let myPost = await db.collection("posts").insertOne(bodyObject);
+      res.json(myPost.ops[0]);
+      break;
+    case "GET":
+      const allPosts = await db.collection("allPosts").find({}).toArray();
+      res.json({ status: 200, data: allPosts });
+      break;
+  }
+}
+export async function getServerSideProps(context) {
+  let res = await fetch("http://localhost:3000/api/posts", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  let allPosts = await res.json();
+
+  return {
+    props: { allPosts },
+  };
+}
