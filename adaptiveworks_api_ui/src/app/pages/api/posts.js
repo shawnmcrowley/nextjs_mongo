f83e234af7e313a767fa550fda3db943d8b6a64c
Shawn M. Crowley <scrowley@buffalo.edu>
2024-06-07 18:47:05 -0400

    Json Entities for Adaptive Works

--- adaptiveworks_api_ui/src/app/pages/api/posts.js
+++ adaptiveworks_api_ui/src/app/pages/api/posts.js
@@ -4,7 +4,7 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
-  const db = client.db("nextjs-mongodb-demo");
+  const db = client.db("nextjs-mongodb");
   switch (req.method) {
     case "POST":
       let bodyObject = JSON.parse(req.body);
