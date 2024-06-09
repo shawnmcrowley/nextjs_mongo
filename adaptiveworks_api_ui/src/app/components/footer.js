d2b73d92440f630cceeb60a598f37028943cc0c5
Shawn M. Crowley <scrowley@buffalo.edu>
2024-06-09 08:00:32 -0400

    Added Layout

---
+++ adaptiveworks_api_ui/src/app/components/footer.js
@@ -0,0 +1,7 @@
+export default function footer () {
+    return(
+        <div>
+            Source Code View <a href="https://github.com/shawnmcrowley/nextjs_mongo/tree/main/adaptiveworks_api_ui">Source Code Repository</a>
+        </div>
+    )
+}
